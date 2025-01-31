f630c3910aaaee658413696fd4388f41e4271c4c
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-01-31 09:53:03 -0800

    Commit Message

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ sns.set_theme()
 fig, ax = plt.subplots(figsize=(12, 12))
 
 
-gradient_palette = sns.color_palette("rocket", n_colors=6, as_cmap=True)
+gradient_palette = sns.color_palette("viridis", n_colors=len(school_summary))
 
 # Create a bar plot of Schools with the most prospects
 sns.barplot(
