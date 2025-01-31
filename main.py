6cc9e28b7dc8054c4cf20a56089e026977427157
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-01-30 23:58:56 -0800

    Commit Message

--- main.py
+++ main.py
@@ -154,13 +154,17 @@ st.title("NBA Prospect Schedule")
 st.text("Upcoming NCAA games featuring top 60 NBA draft prospects.")
 draft_with_games = draft_with_games[['Rank', 'Team', 'Player', 'School','DATE', 'TIME', 'AWAY', 'HOME']]
 
-url = "https://www.nbadraft.net/nba-mock-drafts/?year-mock=2025"
-st.write("[nbadraft.net mock draft board](%s)" % url)
 
 
 # Display full draft board with upcoming games
 st.header("Draft Board with Next Games")
 st.text("2025 NBA Mock Draft order with current draft order with their next scheduled NCAA game.")
+url = "https://www.nbadraft.net/nba-mock-drafts/?year-mock=2025"
+st.write("[nbadraft.net mock draft board](%s)" % url)
+single_date = date.today() + timedelta(days=1)  # Start with tomorrow
+date_str = single_date.strftime("%Y%m%d")
+url = f"https://www.espn.com/mens-college-basketball/schedule/_/date/{date_str}"
+st.write("[espn.com ncaa schedule](%s)" % url)
 st.dataframe(draft_with_games.drop_duplicates(subset=['Rank', 'Player', 'School']), hide_index=True)
 
 # Display in Streamlit
