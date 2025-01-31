450f13015c8d22b0b0fb57347c3a30fc8e9b1231
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-01-31 00:39:29 -0800

    Commit Message

--- main.py
+++ main.py
@@ -225,5 +225,5 @@ url = f"https://www.espn.com/mens-college-basketball/schedule/_/date/{date_str}"
 st.write("[espn.com ncaa schedule](%s)" % url)
 
 
-school_summary = draft_df.groupby('School')['Player'].count()
+school_summary = draft_df.groupby(['School'])['Player'].count()
 st.dataframe(school_summary, hide_index=True)
