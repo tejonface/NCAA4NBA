f613178be95149804345cb50fa37e6b5e4ba203c
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-02-01 10:55:18 -0800

    Commit Message

--- main.py
+++ main.py
@@ -240,10 +240,15 @@ school_summary = school_summary.sort_values(by='Total', ascending=False)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Set the theme for Seaborn
-#sns.set_context("notebook", font_scale=1.5)
-#sns.color_palette("flare")
+sns.color_palette("flare")
+pal = sns.color_palette("Greens_d", len(data))
+rank = school_summary.argsort().argsort()  # http://stackoverflow.com/a/6266510/1628638
+sns.barplot(x=school_summary.index, y=school_summary['Total'], palette=np.array(pal[::-1])[rank])
+
+plt.show()
 # Create a figure and axis
 fig, ax = plt.subplots(figsize=(12, 12))
 
