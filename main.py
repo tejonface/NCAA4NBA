11014b28f0fb65a76f92d14a97d6f0379da53432
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-02-02 13:51:53 -0800

    Commit Message

--- main.py
+++ main.py
@@ -291,3 +291,6 @@ single_date = date.today() + timedelta(days=1)  # Start with tomorrow
 date_str = single_date.strftime("%Y%m%d")
 url = f"https://www.espn.com/mens-college-basketball/schedule/_/date/{date_str}"
 st.write("[espn.com ncaa schedule](%s)" % url)
+
+url= "https://www.jstew.info"
+st.write("[created by jstew.info](%s)" % url)
