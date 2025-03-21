a5fb49360f45f23dd4f8bbe5d5a403a198a50d44
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-03-21 16:04:48 -0700

    Commit Message

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # =================================================================== Scrape NBA Draft Board
 # Function to scrape NBA draft board tables
-@st.cache_data
+@st.cache_data(ttl=3600)
 def scrape_nba_mock_draft(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -45,7 +45,7 @@ draft_df = scrape_nba_mock_draft(draft_url)
 # =================================================================== Scrape NCAA Schedule
 
 # Function to scrape NCAA schedule
-@st.cache_data
+@st.cache_data(ttl=3600)
 def scrape_ncaa_schedule():
     combined_df = pd.DataFrame()
 
