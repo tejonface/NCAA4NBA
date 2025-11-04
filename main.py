1dcc21e32a3e2b23173fa030af78d3c0f3dbc46f
jstewart3631 <77296488+jstewart3631@users.noreply.github.com>
2025-11-03 16:33:30 -0800

    Commit Message

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def scrape_ncaa_schedule():
     combined_df = pd.DataFrame()
 
     for i in range(7):  # Loop through the next 7 days
-        single_date = date.today() + timedelta(days=0 + i)  # Start with today
+        single_date = date.today()-1 + timedelta(days=0 + i)  # Start with today
         # sample_date = date(2025, 11,3) + timedelta(days=i)
         date_str = single_date.strftime("%Y%m%d")
         #  date_str = sample_date.strftime("%Y%m%d")
